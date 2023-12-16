cd561d8a25a6e07628b6beeff3fcc5f8a1a16ce3
Jasef HA <75513613@certus.edu.pe>
2023-12-16 11:59:40 -0500

    corrigiendo app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
